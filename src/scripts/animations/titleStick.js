d28dc61ccfc2715980106df011704a0ed0b882fa
Quinten <quintenclaes7@gmail.com>
2025-05-17 17:48:56 +0200

    added title stick animation

--- src/scripts/animations/titleStick.js
+++ src/scripts/animations/titleStick.js
@@ -1,3 +1,34 @@
-export default function initTitleStickAnimation(){
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
+export default function initTitleStickAnimation() {
+  const $container = document.querySelectorAll("[data-animation='title-pin'");
+
+  $container.forEach(($el) => {
+    const $title = $el.querySelectorAll("[data-animation-child='title-pin']");
+
+    $title.forEach(($child) => {
+      stickAnimation($el, $child);
+    });
+  });
+}
+
+function stickAnimation($el, $child) {
+  const containerHeight = $el.offsetHeight;
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $el,
+      start: "top top",
+      end: `+=${containerHeight}`,
+      scrub: true,
+      pin: true,
+      markers: true,
+    },
+  });
+
+  tl.to($child, {
+    y: containerHeight / 4,
+  });
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 import initMouseAnimation from "./animations/mouse";
 import initPageLoaderAnimation from "./animations/pageLoader";
+import initTitleStickAnimation from "./animations/titleStick";
 
 document.addEventListener("DOMContentLoaded", () => {
   initMouseAnimation();
