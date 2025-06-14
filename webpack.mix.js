171912615553889042e6ddd6d04b63106a4e86d8
Quinten <quintenclaes7@gmail.com>
2025-06-14 14:00:13 +0200

    Making sure webpackmix and postCss is correct

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,10 @@
 let mix = require("laravel-mix");
 
-mix.js("src/scripts/main.js", "build").setPublicPath("build").version();
-
-mix.postCss("src/styles/main.css", "build").version();
-
-mix.browserSync({
-  server: true,
-});
+mix
+  .js("src/scripts/main.js", "build")
+  .setPublicPath("build")
+  .postCss("src/styles/main.css", "build")
+  .version()
+  .browserSync({
+    server: true,
+  });
